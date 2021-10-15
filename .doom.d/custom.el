29002e4972dffdee47f354d93a382724a4ef0f91
Brandon Shega <b.shega@gmail.com>
2021-10-15 11:37:43 -0400

    Fixing stuff

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -1,10 +1,10 @@
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(seoul256-theme doom-themes evil use-package)))
+ '(custom-safe-themes
+   '("e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
